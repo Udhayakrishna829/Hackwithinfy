a5e96f4969f394d1636583be29c30e75fd73afe9
UDHAYA KRISHNA M <54506440+Udhayakrishna829@users.noreply.github.com>
2020-07-17 16:04:56 +0530

    Update converting_sequence.py

--- converting_sequence.py
+++ converting_sequence.py
@@ -1,11 +1,12 @@
+n=2*int(input())
 k=list(input())
 l=[]
 c=0
-for i in range(len(k)):
+for i in range(n):
     if(k[i]=='('):
         l.append('(')
     elif(l==[]):
-        for j in range(i+1,len(k)):
+        for j in range(i+1,n):
             c+=1
             if(k[j]=='('):
                 (k[i],k[j])=(k[j],k[i])
